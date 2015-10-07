1d27574a1049ff0ee8f8519becfe315d724e5cb7
Connor Smith <con.smith13@gmail.com>
2015-10-08 07:07:14 +0900

    optimizes product function for space

--- interview-cake/c2_array_of_products.py
+++ interview-cake/c2_array_of_products.py
@@ -1,7 +1,37 @@
 def main():
-    sampleInput = [1, 7, 3, 4, 0]
+    sampleInput = [1, 7, 3, 4]
 
-    print("Input: %s produced Output: %s" %(sampleInput, get_product_array_v2(sampleInput)))
+    print("Input: %s produced Output: %s" %(sampleInput, get_product_array_v3(sampleInput)))
+
+def get_product_array_v3(inputList):
+    ''' 
+    Returns a list of the products of all elements of the input 
+    list, except the one at the index itself.
+    
+    Example
+      input [1, 7, 3, 4]
+      output [7*3*4, 1*3*4, 1*7*4, 1*7*3]
+
+    Requires two iterations through the input list and only
+    allocates space for the output array, which is an 
+    improvement both in time and space over the first solution.
+    '''
+
+    outputList = [1] * len(inputList)
+    
+    # get all of the products before the index
+    prod = 1
+    for i in range(len(inputList)-1):
+        prod *= inputList[i]
+        outputList[i+1] = prod
+
+    # get all of the products after the index
+    prod = 1
+    for i in range(len(inputList)-1, -1, -1):
+        outputList[i] *= prod
+        prod *= inputList[i]
+
+    return outputList
 
 def get_product_array_v2(inputList):
 
