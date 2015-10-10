2377af94a6407ff1eb8f3659c5af62bdf88194ff
Connor Smith <con.smith13@gmail.com>
2015-10-10 23:11:22 +0900

    adds dynamic programming solution to coin problem

--- interview-cake/c5_coin_denominations.py
+++ interview-cake/c5_coin_denominations.py
@@ -1,27 +1,52 @@
-def findCombos(amount, denominations):
-
-    if amount == 0:
+def findCombos(amountLeft, denominations):
+    '''
+    This is the top down approach, and it makes several repeated
+    function calls, wasting time.
+    '''
+    if amountLeft == 0:
         return 1
 
-    # no denominations, no coins
-    if amount < 0 or denominations == []:
+    # no denominations remaining, or overspent
+    if amountLeft < 0 or denominations == []:
         return 0
 
-    print("In findCombos: a(%s), d(%s)"%(amount, denominations))
+    print("In findCombos: a(%s), d(%s)"%(amountLeft, denominations))
 
-    currentCoin, otherCoins = denominations[0], denominations[1:]
+    currentDenom, otherDenoms = denominations[0], denominations[1:]
 
-    comboNum = 0
-    while amount >= 0:
-        comboNum += findCombos(amount, otherCoins)
-        amount -= currentCoin
-    return comboNum
+    numberOfCombinations = 0
+    while amountLeft >= 0:
+        combosReturned = findCombos(amountLeft, otherDenoms)
+        numberOfCombinations += combosReturned
+        amountLeft -= currentDenom
+    return numberOfCombinations
 
+def findCombosBottomUp(amount, denominations):
+    '''
+    Avoids the call stack memory issues associated with using 
+    large denominations or high input values.
+    '''
+    combinations = [0] * (amount+1);
+    combinations[0] = 1
+
+    for coin in denominations:
+        for higherAmount in range(coin, amount+1):
+            remainder = higherAmount-coin
+            combinations[higherAmount] += combinations[remainder]
+ 
+    return combinations[amount]
+
+'''
+To-do:
+
+Use memoization to improve the efficiency of the recursive method
+Try to return the possible combinations as well as the number of combinations
+'''
 
 def main():
     testCase = (4, [1,2,3])
 
-    combinations = findCombos(4, [1,2, 3])
+    combinations = findCombosBottomUp(testCase[0], testCase[1])
     print("Using %s to produce %s, there are %s combinations."
         % (testCase[1], testCase[0], combinations))
 
