7c61aa013536118fd994407ff4a83cf437aa7a77
Connor Smith <con.smith13@gmail.com>
2015-10-19 00:22:00 +0900

    implements dynamic programming solution to cake thief problem

--- interview-cake/c16_cake_thief.py
+++ interview-cake/c16_cake_thief.py
@@ -1,3 +1,44 @@
+def getBagValueOptimized(bag, cakeTupleList):
+    '''
+    Best case: fill the entire bag with the highest value density cake
+
+    Problem: what if that cake doesn't fit in evenly
+
+    Solution: Build a LUT for each integer kilogram the bag holds
+    '''
+    
+    augmentedCakeList = []
+
+    for cakeTuple in cakeTupleList:
+        if cakeTuple[0] == 0:
+            if cakeTuple[1] > 0:
+                return int('inf')
+        else:
+            augmentedCakeList.append(   (cakeTuple[0],cakeTuple[1],cakeTuple[1]/cakeTuple[0]))
+
+    # cost of O(nlogn) number of cake options
+    augmentedCakeList = sorted(augmentedCakeList, key=lambda val: val[0], reverse = False)
+
+    bestValueList = [0]
+
+    cakeNum = len(augmentedCakeList)
+
+    # cost of O(k) bag size
+    for size in range(1,bag+1):
+        ind = 0
+        currentMax = 0
+        temp = 0
+        while ind < cakeNum-1 and augmentedCakeList[ind][0] <= size:
+            temp = augmentedCakeList[ind][1] + bestValueList[size-augmentedCakeList[ind][0]]
+            if temp > currentMax:
+                currentMax = temp
+            ind += 1
+        bestValueList.append(max(currentMax, bestValueList[size-1]))
+        # print(bestValueList)
+
+
+    return bestValueList[bag]
+
 def getBagValue(bag, cakeTuple):
     # empty cake list case
     if cakeTuple == []:
@@ -31,9 +72,10 @@ def getBagValue(bag, cakeTuple):
 def main():
     cakeTuple = [(7, 160), (3, 90), (2, 15),(0,0)]
     # cakeTuple = [(10, 160),(9, 165),(2,10)]
-    bagSize = 20
+    bagSize = 10
 
     print(getBagValue(bagSize, cakeTuple))
+    print(getBagValueOptimized(bagSize,cakeTuple))
 
 if __name__ == "__main__":
     main()
