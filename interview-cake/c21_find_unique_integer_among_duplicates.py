3ceaf950d77363b0acc0be338c25c88727f85d18
Connor Smith <con.smith13@gmail.com>
2015-10-21 00:05:57 +0900

    add XOR solution for identifying unique integer in array

--- interview-cake/c21_find_unique_integer_among_duplicates.py
+++ interview-cake/c21_find_unique_integer_among_duplicates.py
@@ -14,13 +14,23 @@ def getUniqueID(inArray):
         else:
             pairDictionary[quadID] = 1
 
-    return pairDictionary.keys()    
+    return pairDictionary.keys()
+
+def getUniqueIDSpaceOptimized(inArray):
+    a = 0
+
+    for quadID in inArray:
+        a = a ^ quadID
+
+    return a
 
 def main():
     # should return 9 as the unique integer
     testArray = [1,2,5,3,2,5,3,1,9,3,4,3,4]
 
     print("Unique ID was: %s"%(getUniqueID(testArray)))
+    print("With space optimized solution, unique ID was: %s"%(getUniqueIDSpaceOptimized(testArray)))
+
 
 if __name__ == "__main__":
     main()
